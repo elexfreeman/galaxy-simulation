2c3072b34092cc50fbac87228ec345037166afca
Alexander Polozov <polozov@alkor.co.ru>
2022-12-03 19:54:35 +0400

    gpu

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,6 @@
 export const MAX_Y = 1000;
 export const MAX_X = 1000;
-export const MAX_DOTS = 20000;
+export const MAX_DOTS = 10000;
 
 export const MAX_DOT_X = 1000;
 export const MAX_DOT_Y = 1000;
--- src/index.js
+++ src/index.js
@@ -1,123 +1,66 @@
-import {GPU} from 'gpu.js';
 import {Vector} from './vector';
-import {Body} from './body';
 import {GeneratorCircle} from './bodyGenerator';
-import {CalculatorWorker} from './calculatorWorker';
+import {kernel} from './gpu-core2'
 import * as C from './consts';
 
+const workerMassCenter = new Worker(
+  new URL('./workerMassCenter.js', import.meta.url),
+  {type: 'module'}
+);
 
 const ctx = document.getElementById('canvas').getContext('2d');
 
-let dataX = [];
-let dataY = [];
-let dataVx = [];
-let dataVy = [];
+let dataArr = [];
+
+let centerMassVector = new Vector(0, 0);
 
 const bodyGenerator = new GeneratorCircle();
 let bodyList = bodyGenerator.generate();
 
-const initData = () => {
+const initData2 = () => {
   for (let k = 0; k < bodyList.length; k++) {
-    dataX.push(bodyList[k].coord.x);
-    dataY.push(bodyList[k].coord.y);
-    dataVx.push(bodyList[k].velocity.x);
-    dataVy.push(bodyList[k].velocity.y);
+    dataArr.push([
+      bodyList[k].coord.x,
+      bodyList[k].coord.y,
+      bodyList[k].velocity.y,
+      bodyList[k].velocity.y
+    ]);
   }
 }
-
-const gpu = new GPU();
-const kernel = gpu.createKernel(function (GG, x, y, vx, vy, len) {
-  let newX = 0;
-  let newY = 0;
-  let newVX = vx[this.thread.x];
-  let newVY = vy[this.thread.x];
-  //
-  let FX = 0;
-  let FY = 0;
-  let R = 0
-  let xMinus = 0;
-  let yMinus = 0;
-
-  for (let k = 0; k < len; k++) {
-    if (k != this.thread.x) {
-
-      // find F
-      xMinus = x[k] - x[this.thread.x];
-      yMinus = y[k] - y[this.thread.x];
-
-      //find Radius
-      R = sqrt((xMinus * xMinus) + (yMinus * yMinus));
-
-      // finde force
-      FX = GG * xMinus / (R * R);
-      FY = GG * yMinus / (R * R);
-
-      // F + V
-      newVX = FX + newVX;
-      newVY = FY + newVY;
-
-    }
-  }
-  // coord++
-  newX = newVX + x[this.thread.x];
-  newY = newVY + y[this.thread.x];
-
-  return [newX, newY, newVX, newVY];
-}).setOutput([C.MAX_DOTS]);
 
 
 
 function init() {
-  initData();
+  initData2();
   ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
   window.requestAnimationFrame(draw);
 }
 
-const body = new Body(new Vector(0, 0), 1);
-const calc = new CalculatorWorker(bodyList, 0, 1);
-
-const getCenterMassVector = () => {
-
-  const centerMassVector = new Vector(0, 0);
-
-  for (let k = 0; k < C.MAX_DOTS; k++) {
-    centerMassVector.x += dataX[k];
-    centerMassVector.y += dataY[k];
-  }
-  centerMassVector.x = (centerMassVector.x / C.MAX_DOTS) - (window.innerWidth / 2);
-  centerMassVector.y = (centerMassVector.y / C.MAX_DOTS) - (window.innerHeight / 2);
-
-  return centerMassVector;
-}
-
-function draw() {
+async function draw() {
   ctx.globalCompositeOperation = 'destination-over';
   ctx.clearRect(0, 0, window.innerWidth, window.innerHeight); // clear canvas
-  ctx.restore();
+  workerMassCenter.postMessage({
+    dataArr,
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
 
+  var start = new Date();
+  dataArr = kernel(C.G, C.MAX_DOTS, dataArr);
 
-  // const centerMassVector = calc.getCenterMassVector();
-  const centerMassVector = getCenterMassVector();
-  const outData = kernel(C.G, dataX, dataY, dataVx, dataVy, C.MAX_DOTS);
+  var time = new Date() - start;
+  let x = 0;
+  let y = 0;
 
-  dataX = [];
-  dataY = [];
-  dataVx = [];
-  dataVy = [];
+  ctx.strokeStyle = '#ffffff'
+  ctx.fillStyle = '#ffffff'
+  ctx.font = '48px serif';
+  ctx.fillText(time, 100, 100);
 
   for (let k = 0; k < C.MAX_DOTS; k++) {
-    dataX.push(outData[k][0]);
-    dataY.push(outData[k][1]);
-    dataVx.push(outData[k][2]);
-    dataVy.push(outData[k][3]);
-  }
-
-  //  console.log(dataX, dataY)
-
-  for (let k = 0; k < dataX.length; k++) {
-    const x = dataX[k];
-    const y = dataY[k];
+    x = dataArr[k][0];
+    y = dataArr[k][1];
     ctx.beginPath();
     ctx.strokeStyle = '#ffffff'
     ctx.arc(x - centerMassVector.x, y - centerMassVector.y, 3, 0, Math.PI * 2, false);
@@ -125,21 +68,19 @@ function draw() {
     ctx.stroke();
   }
 
-  window.requestAnimationFrame(draw);
 }
 
 
-//myWorker.onmessage = (e) => {
-//  ctx.globalCompositeOperation = 'destination-over';
-//  ctx.clearRect(0, 0, C.MAX_X, C.MAX_Y); // clear canvas
-//  ctx.restore();
-//
-//
-////  window.requestAnimationFrame(draw);
-//}
+workerMassCenter.onmessage = (e) => {
+  centerMassVector = e.data;
+  window.requestAnimationFrame(draw);
+};
+
 
 window.addEventListener('resize', function (event) {
   ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
 }, true);
+
 init();
+
