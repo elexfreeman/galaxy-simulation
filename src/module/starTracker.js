b66dc9049b47829d375322649b897f8b6a8d9b16
Alexander Polozov <polozov@alkor.co.ru>
2022-12-18 01:05:16 +0400

    add coord net

--- src/module/starTracker.js
+++ src/module/starTracker.js
@@ -131,7 +131,7 @@ export const drawTraking = (w, h, zoom, starIdx, ctx, image, color) => {
   // drawImage 
   const ship = new Vector(w / 2, h - offsetButtom - 40);
   if (image) {
-    ctx.drawImage(image, ship.x - 6, ship.y, 15, 25);
+    ctx.drawImage(image, ship.x - 8, ship.y, 15, 25);
   }
 
   // global map target
@@ -162,52 +162,94 @@ export const drawTraking = (w, h, zoom, starIdx, ctx, image, color) => {
 
 }
 
-export const drawTrakingNet = (w, h, zoom, starIdx, ctx, step, color) => {
+export const drawTrakingNet = (w, h, zoom, starIdx, ctx, step) => {
   if (!ctx) return;
 
-  const osX = new Vector(1, 0);
+  const oxColorMain = 'rgb(200 255 174 / 52%)';
+  const oxColor = 'rgb(200 255 174 / 15%)';
+
+  const osXnav = new Vector(1, 0);
   const offset = new Vector(w / 2, h / 2);
   const offsetButtom = h / 5;
   const centerMassVector = new Vector(window.dataArr[starIdx][0], window.dataArr[starIdx][1]);
   let vecV = new Vector(window.dataArr[starIdx][2], window.dataArr[starIdx][3]);
   let vecXY = new Vector(0, 0);
-  const deg = Vector.angle2V(osX, vecV);
+  const deg = Vector.angle2V(osXnav, vecV);
   let field = 0;
+  const add = new Vector(0, offsetButtom)
 
   const center = new Vector(w / 2, h / 2);
+  let dOsX1;
+  let dOsX2;
+  let dOsY1;
+  let dOsY2;
 
-  let dOsX1 = new Vector(0, h / 2);
-  let dOsX2 = new Vector(w, h / 2);
+  const addWH = 100;
 
-  let dOsY1 = new Vector(w / 2, 0);
-  let dOsY2 = new Vector(w / 2, h);
+  for (let k = 0; k < step; k++) {
+
+    // OX
+    dOsX1 = new Vector(-addWH, (k * step) + h / 2);
+    dOsX2 = new Vector(w + addWH, (k * step) + h / 2);
+    dOsX1 = Vector.rotateVector(dOsX1, center, deg);
+    dOsX2 = Vector.rotateVector(dOsX2, center, deg);
+    dOsX1 = Vector.add(dOsX1, add);
+    dOsX2 = Vector.add(dOsX2, add);
+
+    ctx.beginPath();
+    ctx.strokeStyle = oxColor;
+    ctx.fillStyle = oxColor;
+    ctx.moveTo(dOsX1.x, dOsX1.y);
+    ctx.lineTo(dOsX2.x, dOsX2.y);
+    ctx.closePath();
+    ctx.stroke();
+
+    dOsX1 = new Vector(-addWH, (h / 2) - (k * step));
+    dOsX2 = new Vector(w + addWH, (h / 2) - (k * step));
+    dOsX1 = Vector.rotateVector(dOsX1, center, deg);
+    dOsX2 = Vector.rotateVector(dOsX2, center, deg);
+    dOsX1 = Vector.add(dOsX1, add);
+    dOsX2 = Vector.add(dOsX2, add);
+
+    ctx.beginPath();
+    ctx.strokeStyle = oxColor;
+    ctx.fillStyle = oxColor;
+    ctx.moveTo(dOsX1.x, dOsX1.y);
+    ctx.lineTo(dOsX2.x, dOsX2.y);
+    ctx.closePath();
+    ctx.stroke();
+
+    // OY
+    dOsY1 = new Vector((k * step) + (w / 2), -addWH);
+    dOsY2 = new Vector((k * step) + (w / 2), h + addWH);
+    dOsY1 = Vector.rotateVector(dOsY1, center, deg);
+    dOsY2 = Vector.rotateVector(dOsY2, center, deg);
+    dOsY1 = Vector.add(dOsY1, add);
+    dOsY2 = Vector.add(dOsY2, add);
+
+    ctx.beginPath();
+    ctx.strokeStyle = oxColor;
+    ctx.fillStyle = oxColor;
+    ctx.moveTo(dOsY1.x, dOsY1.y);
+    ctx.lineTo(dOsY2.x, dOsY2.y);
+    ctx.closePath();
+    ctx.stroke();
+
+    dOsY1 = new Vector((w / 2) - (k * step), -addWH);
+    dOsY2 = new Vector((w / 2) - (k * step), h + addWH);
+    dOsY1 = Vector.rotateVector(dOsY1, center, deg);
+    dOsY2 = Vector.rotateVector(dOsY2, center, deg);
+    dOsY1 = Vector.add(dOsY1, add);
+    dOsY2 = Vector.add(dOsY2, add);
+
+    ctx.beginPath();
+    ctx.strokeStyle = oxColor;
+    ctx.fillStyle = oxColor;
+    ctx.moveTo(dOsY1.x, dOsY1.y);
+    ctx.lineTo(dOsY2.x, dOsY2.y);
+    ctx.closePath();
+    ctx.stroke();
+  }
 
 
-  dOsX1 = Vector.rotateVector(dOsX1, center, deg);
-  dOsX2 = Vector.rotateVector(dOsX2, center, deg);
-  dOsY1 = Vector.rotateVector(dOsY1, center, deg);
-  dOsY2 = Vector.rotateVector(dOsY2, center, deg);
-
-  const add = new Vector(0, offsetButtom )
-  dOsX1 = Vector.add(dOsX1, add);
-  dOsX2 = Vector.add(dOsX2, add);
-  dOsY1 = Vector.add(dOsY1, add);
-  dOsY2 = Vector.add(dOsY2, add);
-
-  ctx.beginPath();
-  ctx.strokeStyle = color;
-  ctx.fillStyle = color;
-  ctx.moveTo(dOsX1.x, dOsX1.y);
-  ctx.lineTo(dOsX2.x, dOsX2.y);
-  ctx.closePath();
-  ctx.stroke();
-
-
-  ctx.beginPath();
-  ctx.strokeStyle = 'blue';
-  ctx.fillStyle = 'blue';
-  ctx.moveTo(dOsY1.x, dOsY1.y);
-  ctx.lineTo(dOsY2.x, dOsY2.y);
-  ctx.closePath();
-  ctx.stroke();
 }
